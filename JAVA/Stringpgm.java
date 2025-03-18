ed62ce8c4650881513ce7e79626c3f1884de9f96
Subisuresh321 <subisuresh69@gmail.com>
2025-03-18 10:13:52 +0530

    added

---
+++ JAVA/Stringpgm.java
@@ -0,0 +1,12 @@
+import java.lang.String;
+public class Stringpgm {
+    public static void main(String[] args) {
+        String ch="Hello";
+        System.out.println(ch);
+        String s=ch+" world";   
+        System.out.println(s);
+        System.out.println(s.length());
+
+        
+    }
+}
---
+++ JAVA/Stringpgmtwo.java
@@ -0,0 +1,9 @@
+
+public class Stringpgmtwo {
+    public static void main(String[] args) {
+        StringBuffer str=new StringBuffer("abc");
+        System.out.println(str);
+        System.out.println(str.append("ok"));
+        System.out.println(str.insert("okk"));
+    }
+}
