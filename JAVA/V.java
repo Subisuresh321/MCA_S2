24331958ccd4fba9deb79a8b78db32c34557c647
Subisuresh321 <subisuresh69@gmail.com>
2025-03-24 12:13:12 +0530

    added

---
+++ JAVA/V.java
@@ -0,0 +1,16 @@
+public class V {
+    static void bs(int arr[],int k){
+        int low=0;
+        int high=arr.length-1;
+        while(low<high)
+        {
+            int mid=(low+high)/2;
+            if(mid==k)
+            sout
+        }
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
